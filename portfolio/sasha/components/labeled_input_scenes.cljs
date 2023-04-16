99e0b31c9f154b53b64df718519d252515fedb74
Christian Johansen <christian@cjohansen.no>
2023-04-16 16:40:43 +0200

    Add labeled input that does responsiveness via code

--- portfolio/sasha/components/labeled_input_scenes.cljs
+++ portfolio/sasha/components/labeled_input_scenes.cljs
@@ -1,5 +1,5 @@
 (ns sasha.components.labeled-input-scenes
-  (:require [sasha.components.labeled-input :refer [LabeledInput]]
+  (:require [sasha.components.labeled-input :refer [LabeledInput LabeledInput2]]
             [portfolio.dumdom :refer-macros [defscene]]))
 
 (defscene empty-input
@@ -29,3 +29,28 @@
     :label "Your email"
     :value "christian@kodemaker.no"
     :disabled? true}))
+
+(defn render-scene [data opt]
+  (LabeledInput2
+   (cond-> data
+     (and (number? (:viewport/width opt))
+          (< (:viewport/width opt) 600))
+     (assoc :mobile? true))))
+
+(defscene input2-with-text
+  "`LabeledInput2` uses runtime data for responsive design instead
+   of relying on CSS media-queries."
+  :params {:id "email-4"
+           :label "Your email"
+           :value "christian@kodemaker.no"}
+  render-scene)
+
+(defscene input2-with-warning
+  :params {:id "email-5"
+           :label "Your email"
+           :value "christian@kodemaker."
+           :error? true
+           :messages [{:kind :error
+                       :icon :sasha.icons/warning
+                       :text "Email seems incomplete"}]}
+  render-scene)
--- src/sasha/components/labeled_input.cljs
+++ src/sasha/components/labeled_input.cljs
@@ -20,3 +20,34 @@
            (icons/render icon {:size 16
                                :style {:margin-right 8}}))
          text])])])
+
+(defcomponent LabeledInput2 [{:keys [id label value on-input error? messages disabled? mobile?]}]
+  [:div
+   {:style {:color (when error? "var(--error)")}}
+   [:div {:style {:display (when-not mobile? "flex")
+                  :align-items "center"}}
+    [:label {:for id
+             :style (if mobile?
+                      {:display "block"
+                       :margin "0 0 10px"}
+                      {:flex "0 0 30%"})}
+     label]
+    [:input.input
+     {:id id
+      :value value
+      :disabled disabled?
+      :type "text"
+      :style {:background (when disabled? "var(--input-disabled-bg)")
+              :color (when disabled? "var(--input-disabled-fg)")}}]]
+   (when (seq messages)
+     [:div {:style {:margin-left (when-not mobile? "30%")
+                    :font-size "0.9rem"}}
+      (for [{:keys [kind text icon]} messages]
+        [:div {:class kind
+               :style {:display "flex"
+                       :align-items "center"
+                       :margin "10px 0"}}
+         (when icon
+           (icons/render icon {:size 16
+                               :style {:margin-right 8}}))
+         text])])])
